2ace3895131a6b8c2a0c8dec5ddf9bcba9ea76f5
Hans Petter Årvik Bastiansen <hpbastiansen@gmail.com>
2022-05-03 12:51:00 +0200

    Started on boss scene

---
+++ Assets/BossController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    public void Trigger()
+    {
+        // Start boss movement (terrain movement)
+    }
+
+    public void MoveBoss()
+    {
+        // If player is grounded, move terrain. If not, move boss itself
+    }
+
+    public void CreateSegments()
+    {
+        // Always make sure there is segments to left and right of player
+    }
+}
