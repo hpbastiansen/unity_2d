13626b6e4107cc3aee678b0ddfa135d7d9cffa46
Hans Petter Årvik Bastiansen <hpbastiansen@gmail.com>
2022-05-06 19:30:00 +0200

    Added worm indication

--- Assets/WormSpawnerBoss.cs
+++ Assets/WormSpawnerBoss.cs
@@ -7,6 +7,7 @@ public class WormSpawnerBoss : MonoBehaviour
     private Vector3 _pointTo;
     private float _timeSinceSpawn = 0;
     private float _lastCheck = 0;
+    private GameObject _currentParticles;
 
     [Header("Angle")]
     [SerializeField] private float _angleMin;
@@ -18,6 +19,7 @@ public class WormSpawnerBoss : MonoBehaviour
 
     [Header("Other")]
     [SerializeField] private GameObject _worm;
+    [SerializeField] private GameObject _particles;
 
     // Update is called once per frame
     void Update()
@@ -36,6 +38,17 @@ public class WormSpawnerBoss : MonoBehaviour
     void SpawnWorm()
     {
         _timeSinceSpawn = 0f;
+        
+
+        StartCoroutine(Spawn());
+        //Invoke("Spawn", 1f);
+    }
+
+    IEnumerator Spawn()
+    {
+        _currentParticles = Instantiate(_particles, new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z), Quaternion.identity);
+        yield return new WaitForSeconds(1f);
+        Destroy(_currentParticles);
 
         float _angle = Random.Range(_angleMin, _angleMax) * Mathf.Deg2Rad;
         Vector3 _direction = new Vector3(Mathf.Cos(_angle), Mathf.Sin(_angle), 0);
